53d70426a89d967e7c757dac0e9ee429c02ecc59
Pablo Maldonado <public.maldonado@gmail.com>
2019-10-09 11:58:26 -0300

    Nodegit installed & did it some functions

---
+++ src/renderer/git/allGit.js
@@ -0,0 +1,42 @@
+import NodeGit, { Repository, Diff, Reference } from "nodegit";
+
+export const getRepository = async path => {
+	const repository = await Repository.open(path);
+	return repository;
+};
+
+export const getCurrentBranch = async path => {
+	const repo = await Repository.open(path);
+
+	return await repo.getCurrentBranch();
+};
+
+export const getCurrentBranchName = async path => {
+	const repo = await Repository.open(path);
+
+	const currentBranch = await repo.getCurrentBranch();
+
+	return currentBranch.shorthand();
+};
+
+export const getStatus = async path => {
+	const repo = await Repository.open(path);
+	const statuses = await repo.getStatus();
+
+	return statuses;
+};
+
+export const getDiff = async path => {
+	const repo = await NodeGit.Repository.open(path);
+	const head = await repo.getHeadCommit();
+	if (!head) {
+		return [];
+	}
+	// const diff = await Diff.treeToIndex(repo, await head.getTree(), null);
+	const diff = await Diff.indexToWorkdir(repo, null, {
+		flags:
+			Diff.OPTION.SHOW_UNTRACKED_CONTENT | Diff.OPTION.RECURSE_UNTRACKED_DIRS
+	});
+	const patches = await diff.patches();
+	console.log(patches.map(patch => patch.newFile().path()));
+};
